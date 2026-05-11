2f8b66a0a6c2ee701a9f04475487b0f3eb66d1c7
spebelgenenst <sebastian@die-sperlinge.de>
2026-05-11 19:37:22 +0200

    improved send_message command

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    await tree.sync()#guild=discord.Object(id=1430225351455543418))
-    print(f"Ready! {client.user}")
+    await tree.sync()
+    print(f"Bot logged in as {client.user}")
 
-@tree.command(
-    name="send_message",
-    description="let a person send a message",
-    #guild=discord.Object(id=1430225351455543418)
-)
+@tree.command(name="send_message", description="let a person send a message")
 async def first_command(interaction: discord.Interaction, user: discord.Member, message: str):
+
+    await interaction.response.defer(ephemeral=True)
+    response = await interaction.original_response() 
+
     webhook = await interaction.channel.create_webhook(name="test")
     await webhook.send(content=message, username=user.display_name, avatar_url=user.avatar)
 
-    await webhook.delete()
+    await response.delete()
 
-    await interaction.response.send_message(f"message send :3", ephemeral=True)
+    await webhook.delete()
 
 client.run(credentials["discordBotToken"])
