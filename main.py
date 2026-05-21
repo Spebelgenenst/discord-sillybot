2d25c48ec3174ef08e8a1c1dc3509a1251ddbda1
spebelgenenst <sebastian@die-sperlinge.de>
2026-05-21 16:57:43 +0200

    added /petpet and petpet on member join

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import discord
 from discord import app_commands
 import json
-#from petpetgif import petpet
+from petpetgif import petpet
 from typing import Literal
 import random
 
-import torch
-from transformers import pipeline
+#import torch
+#from transformers import pipeline
 
 import asyncio
+
+from io import BytesIO
 
 with open("credentials.json", "r") as f:
     credentials = json.load(f)
@@ -22,7 +24,7 @@ intents.messages = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
+#pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
 
 async def ai(prompt):
     message = [
@@ -38,6 +40,12 @@ async def ai(prompt):
     clean_text = raw_text.split("<|assistant|>")[1].replace(prompt, "").strip()
 
     return clean_text
+
+def pet_pet(image):
+    pet_pet_gif = BytesIO()
+    petpet.make(BytesIO(image), pet_pet_gif)
+    pet_pet_gif.seek(0)
+    return pet_pet_gif
 
 @client.event
 async def on_ready():
@@ -87,6 +95,16 @@ async def predict_message(interaction: discord.Interaction, user: discord.Member
     await loading_message.delete()
     await webhook.delete()
 
+@tree.command(name="petpet", description="pet a user")
+async def petepet_user(interaction: discord.Interaction, user: discord.Member):
+    if not user.avatar:
+        await interaction.response.send_message("user has no avatar", ephemeral=True)
+        return
+    
+    avatar = await user.avatar.read()
+    pet_pet_gif = pet_pet(avatar)
+    await interaction.response.send_message(file=discord.File(pet_pet_gif, filename=f"{user.name}_petpet.gif"))
+
 @client.event
 async def on_message(message):
     if not (client.user.mentioned_in(message) and message.author != client.user):
@@ -96,5 +114,17 @@ async def on_message(message):
     response = await ai(message.content.replace(f"<@{client.user.id}>", "ai"))
     await message.reply(response)
     await message.remove_reaction("⌛", client.user)
+
+@client.event
+async def on_member_join(user):
+    channel = await client.fetch_channel(config["joinMessageChannelId"])
+    if not user.avatar:
+        await interaction.response.send_message("user has no avatar", ephemeral=True)
+        return
+    
+    avatar = await user.avatar.read()
+    pet_pet_gif = pet_pet(avatar)
+
+    await channel.send(file=discord.File(pet_pet_gif, filename=f"{user.name}_petpet.gif"))
 
 client.run(credentials["discordBotToken"])
